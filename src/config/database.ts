638c2a7df00f1514994623a20af8e37eda1a84e2
LuisMtz139 <84791177+LuisMtz139@users.noreply.github.com>
2024-05-16 16:48:46 -0600

    Add files via upload

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,12 +3,12 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT)
 });
 
 export default pool.promise();
