29312c60a4b7c74a4385452502a4b2c1fa4176e1
LuisMtz139 <84791177+LuisMtz139@users.noreply.github.com>
2024-05-17 23:42:02 -0600

    Add files via upload

--- dist/config/database.js
+++ dist/config/database.js
@@ -10,6 +10,7 @@ const pool = mysql2_1.default.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    port: Number(process.env.DB_PORT)
 });
 exports.default = pool.promise();
