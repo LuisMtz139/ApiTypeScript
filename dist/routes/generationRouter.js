6cbc2013a7d13c7212e1a3517d003f85d9f502ef
LuisMtz <LuisMartinezMA12@gmail.com>
2024-05-18 00:03:09 -0600

    nuevos metodos

--- dist/routes/generationRouter.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const generationController_1 = require("../controllers/generationController");
-const router = (0, express_1.Router)();
-router.get('/', generationController_1.getGenerations);
-router.get('/:cohorte', generationController_1.getGenerationDetail);
-router.get('/:cohorte/estadisticas', generationController_1.getGenerationStats);
-exports.default = router;
--- dist/routes/subjectsRouter.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const subjectsController_1 = require("../controllers/subjectsController");
-const subjectController = new subjectsController_1.SubjectController();
-const router = (0, express_1.Router)();
-router.get('/', subjectController.subjectList.bind(subjectController));
-exports.default = router;
